43814356fbc50eee8599254fae846248e3948b08
Sabbrumm <38012216+Sabbrumm@users.noreply.github.com>
2023-04-11 23:16:10 +0300

    Ready
    Все готово!

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,6 @@
-
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,4 +14,23 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_btnHello_clicked()
+{
+    QMessageBox msgbox;
+    QString fio;
+    if (!(ui->FIOInput->text().isEmpty())){
+        msgbox.setWindowTitle("Рад встрече!");
+        fio.append("Привет, ");
+        fio.append(ui->FIOInput->text());
+        fio.append("!");
+    }
+    else{
+        msgbox.setWindowTitle("Таинственный незнакомец!");
+        fio.append("Прости, я не знаю твоего имени...\nНе мог бы ты его для меня ввести?");
+    }
 
+
+    msgbox.setText(fio);
+    msgbox.exec();
+    fio.clear();
+}
